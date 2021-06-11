25f03e48611ac175b09c4e989a1910f38fb8bff3
Geo Lee <geolee1@naver.com>
2021-06-11 13:55:58 +0900

    setting menu add by 박준혁

---
+++ src/console/SettingConsole.py
@@ -0,0 +1,35 @@
+# 몇시간으로 설정할지 질문한다 정수 입력받기
+# 아무것도 입력 안 할시 2시간으로 설정하기
+# db파일 어디다 저장할지 물어본다
+# 입력받는다
+
+def setting_menu():
+    while True:
+        print("=================================")
+        print("환경설정")
+        print("1. 검색 범위 설정")
+        print("2. database 저장 위치")
+        print("=================================")
+
+        try:
+            select = int(input("환경설정에서 설정할 것을 선택해주세요:"))
+        except:
+            print("정수를 입력해주세요.")
+            continue
+
+        if select == 1:
+            set_search_range()
+
+        elif select == 2:
+            set_db_location()
+
+
+def set_search_range():
+    try:
+        int(input("검색할 시간을 설정하세요."))
+    except:
+        print("시간이 경과하여 2시간으로 설정합니다.")
+
+
+def set_db_location():
+    print("원하는 폴더를 정하세요.")
